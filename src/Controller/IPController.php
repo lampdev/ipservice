bc24960c33aa5c05359d414dd3e80dae4187fce0
Ihor Koval <orders@lamp-dev.com>
2020-02-09 09:56:18 +0100

    * adding route

--- src/Controller/IPController.php
+++ src/Controller/IPController.php
@@ -1,6 +1,35 @@
 <?php
+/**
+ * Class IPController
+ */
 
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Routing\Annotation\Route;
 
-class IPController {
+/**
+ * Class IPController
+ * @Route("/")
+ */
+class IPController extends AbstractFOSRestController {
+  /**
+   * REST action which returns client's IP address.
+   * Method: GET, url: /ip.{_format}
+   *
+   * @Rest\Get("/ip.{_format}")
+   *
+   * @return Response
+   */
+  public function ip(): Response {
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+      $ip = $_SERVER['REMOTE_ADDR'];
+    }
 
+    return $this->handleView($this->view($ip));
+  }
 }
