1a7a80c7b2c7fe79fa77913623305f6126d033bc
Ihor Koval <orders@lamp-dev.com>
2020-02-09 10:41:50 +0100

    * response formatting

--- src/Controller/IPController.php
+++ src/Controller/IPController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,9 +21,11 @@ class IPController extends AbstractFOSRestController {
    *
    * @Rest\Get("/ip.{_format}")
    *
+   * @param Response $response instance of symfony response
+   *
    * @return Response
    */
-  public function ip(): Response {
+  public function ip(Request $request, Response $response): Response {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
       $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
@@ -31,6 +34,18 @@ class IPController extends AbstractFOSRestController {
       $ip = $_SERVER['REMOTE_ADDR'];
     }
 
-    return $this->handleView($this->view($ip));
+    $response->headers->add(
+        [
+            'x-hello-world' => 'I.K.'
+        ]
+    );
+    $json = [
+        'ip' => $ip
+    ];
+    if (($name = $request->get('name'))) {
+      $json['greeting'] = 'Hello ' . $name
+    }
+
+    return $this->handleView($this->view($json));
   }
 }
