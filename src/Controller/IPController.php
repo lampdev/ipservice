7eb0c398e633c97ae96e7420eb01a2226843aa09
Ihor Koval <orders@lamp-dev.com>
2020-02-09 10:53:29 +0100

    * response formatting

--- src/Controller/IPController.php
+++ src/Controller/IPController.php
@@ -6,9 +6,9 @@ namespace App\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Request\ParamFetcher;
 
 /**
  * Class IPController
@@ -19,13 +19,14 @@ class IPController extends AbstractFOSRestController {
    * REST action which returns client's IP address.
    * Method: GET, url: /ip.{_format}
    *
-   * @Rest\Get("/ip.{_format}?name={name}")
+   * @Rest\Get("/ip.{_format}")
+   * @Rest\QueryParam(name="name", default=null, nullable=true)
    *
-   * @param string   $name     optional name argument
+   * @param ParamFetcher $paramFetcher  FOR REST bundle param fetcher
    *
    * @return Response
    */
-  public function ip(string $name = null): Response {
+  public function ip(ParamFetcher $paramFetcher): Response {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
       $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
